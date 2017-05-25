a0c91879c7c272b3b521316f3ef4fc35184e6e45
yuxiaobin <179634696@qq.com>
2017-05-25 14:33:28 +0800

    add join table query with/without pagination test case

--- src/test/resources/h2/user.insert.sql
+++ src/test/resources/h2/user.insert.sql
@@ -3,3 +3,8 @@ insert into h2user(test_id, name, test_date, age, price, test_type, version) val
 insert into h2user(test_id, name, test_date, age, price, test_type, version) values (103,'Bob', '2017-4-1 1:1:1', 13, 99.99, 1, 1);
 insert into h2user(test_id, name, test_date, age, price, test_type, version) values (104,'Joe', '2017-2-1 1:1:1', 18, 1.99, 1, 1);
 insert into h2user(test_id, name, test_date, age, price, test_type, version) values (105,'Tony', '2017-2-1 1:1:1', 18, 1.99, 1, 1);
+insert into h2address(addr_id,addr_name,test_id) values(1,'Suzhou', 101);
+insert into h2address(addr_id,addr_name,test_id) values(2,'Beijing', 101);
+insert into h2address(addr_id,addr_name,test_id) values(3,'Shanghai', 101);
+insert into h2address(addr_id,addr_name,test_id) values(4,'Guangzhou', 101);
+insert into h2address(addr_id,addr_name,test_id) values(5,'Hangzhou', 101);
